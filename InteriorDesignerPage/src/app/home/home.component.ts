ab34bb297b3d6323da9263f365fa6606514a8e8f
Khawja Ghouse <31949115+khawja-ghouse@users.noreply.github.com>
2023-07-24 10:30:04 +0530

    Completed landing page, started with about page.

--- InteriorDesignerPage/src/app/home/home.component.ts
+++ InteriorDesignerPage/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  isScrolled: boolean = false;
+  shouldBeVisible: boolean = true;
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    if(window.pageYOffset > 100 && window.pageYOffset < 250){
+      const elementToRemove = this.elementRef.nativeElement.querySelector('#menuBlock');
+      this.renderer.removeChild(elementToRemove.parentNode, elementToRemove);
+      this.shouldBeVisible = false;
+    }else{
+      this.shouldBeVisible = true;
+    }
+    this.isScrolled = window.pageYOffset > 250;
+  }
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
   }
